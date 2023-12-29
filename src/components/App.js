0c3f258a40b100e0c7f800cbf81948e8aa9fb6bc
shree <anirudhamane45@gmail.com>
2023-12-29 11:10:05 -0800

    second try

--- src/components/App.js
+++ src/components/App.js
@@ -2,104 +2,16 @@
 import React from "react";
 import { useState } from "react";
 import './../styles/App.css';
+import Weather from "./Weather";
 
 const App = () => {
 
-  //crating 3 useState() for city, temparature and weather.
-  let[city,setCity]=useState(null);
-  let[temp,setTemp]=useState(25);
-  let[weather,setWeather]=useState("Sunny");
-
-  console.log("city",city);
-  console.log("temp",temp);
-  console.log("weather",weather)
-  // function when use click on the submit button.
-  // const fetchApi=async=()=>
-  // {
-  //   // e.preventDefault();
-  //   // console.log("in handlesubmit function");
-  //   const url=`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=4ebb9418ca605fa1931880e565ec065c`
-  //   const response = await fetch(url);
-  //   const resJson= await response.json();   
-  //   setCity(resJson.main) 
-  // }
-
-  // const fetchApi = async(e) =>
-  // {
-  //   e.preventDefault();
-  //   console.log("in fetchApi")
-  //     const url =`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=4ebb9418ca605fa1931880e565ec065c`;
-  //     const response = await fetch(url);
-  //     const resJson = await response.json();
-  //     setCity(resJson.main);
-  // }
-
-    function handleSubmit(e)
-  {
-    e.preventDefault();
-    console.log('in handle submit')
-    const url=`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=4ebb9418ca605fa1931880e565ec065c`;
-    // const response = await fetch(url);
-    // const resJson = await response.json();
-    // console.log(resJson);
-
-    fetch(url)
-    .then((res)=>res.json())
-    .then((data)=>{
-      
-      setTemp(Math.round(data.main.temp))
-      setWeather(data.weather[0].main)
-
-      console.log(data)      
-    })
-    .catch((err)=>console.log(err))
-
-    setCity("")
-  }
-
+  const weatherData = { temperature: 25, conditions: 'Sunny' };
   return (
-    <div>
-        
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="city">Enter City Name</label>
-            <input
-              type="text"
-              id="city"
-              value={city}
-              onChange={(e)=>setCity(e.target.value)}
-            />
-
-            <button>Submit</button>
-        </form>
-
-
-        {
-          
-         temp && temp>20 &&
-         ( <div>
-           <p> <span style={{color:"red"}}>Temparature: {temp}</span> <span style={{color:"red"}}> Condtions: {weather}</span></p>
-           
-            </div>
-          )
-}
-  {
-          temp && temp<=20 &&
-          (
-            
-            <div>
-              
-              <p> <span style={{color:"blue"}}>Temparature: {temp}</span> <span style={{color:"blue"}}> Condtions: {weather}</span></p>
-          
-           
-            </div>
-             
-
-          )
-        }
-
-        
+    <div className="App">
+      <Weather temperature={weatherData.temperature} conditions={weatherData.conditions} />
     </div>
-  )
-}
+  );
+};
 
 export default App
